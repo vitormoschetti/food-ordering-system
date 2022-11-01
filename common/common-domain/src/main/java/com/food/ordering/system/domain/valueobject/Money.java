b884815ccbb81c47a2cb8299dc7fd62e380abe0d
vitor Alexandre Moschetti <vitor.alexandre.moschetti@everis.nttdata.com>
2022-10-31 21:44:51 -0300

    criando o módulo de common class

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,63 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Money {
+
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public boolean isGreaterThanZero() {
+        return this.validateNotNull() && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(final Money money) {
+        return this.validateNotNull() && this.validateNotNull(money) && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    private boolean validateNotNull() {
+        return this.amount != null;
+    }
+
+    private boolean validateNotNull(final Money money) {
+        return money.getAmount() != null;
+    }
+
+    public Money add(final Money money) {
+        return new Money(this.setScale(this.amount.add(money.getAmount())));
+    }
+
+    public Money subtract(final Money money) {
+        return new Money(this.setScale(this.amount.subtract(money.getAmount())));
+    }
+
+    public Money multiply(final Integer value) {
+        return new Money(this.setScale(this.amount.multiply(new BigDecimal(value))));
+    }
+
+    private BigDecimal setScale(final BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount.equals(money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
